1af8088312e42ca957d7661a1a643c3cb6594aba
Nathamz <62522200+Nathamz@users.noreply.github.com>
2020-03-23 23:51:20 +0700

    Add files via upload

---
+++ ycc/javascript.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    /*! Fades in page on load */
+    $('body').css('display', 'none');
+    $('body').fadeIn(400);
+    
+    });
+    function myFunction() {
+        location.replace("purchase.html")
+      }
